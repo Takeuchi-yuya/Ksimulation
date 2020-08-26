cea4cb2c3035ecb73e9c5512a37dc89a519b47af
tou-fuu <f.tj.0121@jmail.com>
2020-08-27 01:26:38 +0900

    位置情報追加

--- Equationofmotion.py
+++ Equationofmotion.py
@@ -24,29 +24,33 @@ v0 = [float(n) for n in v0]
 v = np.empty((0,3), float)
 v = np.append(v, np.array([v0]), axis=0)
 
-print(v)
-
 for i in range(t_repetition):
     v1 = v[i]
     u1 = np.cross(v1,B)
     k1 = q*(E + u1)/m
+    l1 = v[i]
 
     v2 = v1 + k1*dt/2
     u2 = np.cross(v2,B)
     k2 = q*(E + u2)/m
+    l2 = v[i] + l1/2
 
     v3 = v1 + k2*dt/2
     u3 = np.cross(v3,B)
     k3 = q*(E + u3)/m
+    l3 = v[i] + l2/2
 
     v4 = v1 + k3*dt
     u4 = np.cross(v4,B)
     k4 = q*(E + u4)/m
+    l4 = v[i] + l3
 
     v_tem = v[i] + dt*(k1 + 2*k2 + 2*k3 + k4)/6
     v = np.append(v, np.array([v_tem]), axis=0)
+    x_tem = x[i] + dt*(l1 + 2*l2 + 2*l3 + l4)/6
+    x = np.append(x, np.array([x_tem]), axis=0)
 
-print(v)
+print(x)
 
 
 
