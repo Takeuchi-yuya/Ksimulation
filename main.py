ac7ca7c73ecabd65fb77e8ce1dc1cb02065209f8
tou-fuu <f.tj.0121@jmail.com>
2020-08-29 18:48:21 +0900

    mainの書き換え

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import numpy as np
 from project import *
+
+q, m = inputPar()
+t_repetition = inputTime()
+x = inputPos()
+v = inputVec()
+
+Efield = SampleFunc({"x":-50.0,"y":-50.0,"z":-50.0},{"x":50.0,"y":50.0,"z":50.0},{"x":0.0,"y":0.0,"z":0.0000000000005})
+Bfield = SampleFunc({"x":-50.0,"y":-50.0,"z":-50.0},{"x":50.0,"y":50.0,"z":50.0},{"x":0.0,"y":0.0,"z":0.0000000000025})
+
+x, v = runge(Efield, Bfield, q, m, t_repetition, x, v)
+
+X = np.array([x[i][0] for i in range(t_repetition +1)])
+Y = np.array([x[i][1] for i in range(t_repetition +1)])
+Z = np.array([x[i][2] for i in range(t_repetition +1)])
+
+plams = {"title":"runge","x":X,"y":Y,"z":Z}
+
+oput = OutPut(plams)
+oput.Show()
--- project/InPut.py
+++ project/InPut.py
@@ -1,10 +1,29 @@
 import numpy as np
 import math as ma
 
+def inputPar():
+    kind = input("入射粒子を入力してください(e/p/other)")
+    if kind == "e":
+        q = -1.6/10000000000000000000
+        m = 9.11/10000000000000000000000000000000
+        return q, m
+
+    elif kind == "p":
+        q = 1.6/10000000000000000000
+        m = 1.673/1000000000000000000000000000
+        return q, m
+
+    else:
+        val = int(input("価数を入力してください"))
+        q = val*1.6/10000000000000000000
+        mass = float(input("原子量を入力してください[g/mol]"))
+        Na = 6.02*100000000000000000000000
+        m = mass/(Na/1000)
+        return q, m
+
 def inputTime():
-    t_max = int(input("計算する時間を入力してください。[s]"))
-    t_max = float(t_max)
-    dt = 0.1
+    t_max = float(input("計算する時間を入力してください。[s]"))
+    dt = 0.01
     t_repetition = t_max/dt
     t_repetition = int(t_repetition)
     return t_repetition
@@ -12,17 +31,14 @@ def inputTime():
 
 def inputPos():
     print("初期位置を入力してください。[m]")
-    #int型で読み取ったものをfloat型にして、それぞれの2次配列の先頭に入れる。
-    x0 = list(map(int, input().split()))
-    x0 = [float(n) for n in x0]
+    x0 = list(map(float, input().split()))
     x = np.empty((0,3), float)
     x = np.append(x, np.array([x0]), axis=0)
     return x
 
 def inputVec():
     print("初速度を入力してください。[m/s]")
-    v0 = list(map(int, input().split()))
-    v0 = [float(n) for n in v0]
+    v0 = list(map(float, input().split()))
     v = np.empty((0,3), float)
     v = np.append(v, np.array([v0]), axis=0)
     return v
