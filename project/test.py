73185022615d93f358e7126fc72932968400587a
tou-fuu <f.tj.0121@jmail.com>
2020-09-06 16:50:16 +0900

    動作確認

--- project/test.py
+++ project/test.py
@@ -3,41 +3,24 @@ import math
 from OutPut import OutPut
 from CreatTestdata import SampleFunc
 
+#動作確認はなんとなくアルミニウムイオンで。
 Na = 6.02*100000000000000000000000
+q = 3*1.6/10000000000000000000
+m = (26.98/Na)/1000
 
-kind = input("入射粒子を入力してください(e/p/other)")
-if kind == "e":
-    q = -1.6/10000000000000000000
-    m = 9.11/10000000000000000000000000000000
+Efield = SampleFunc({"x":-0.1,"y":-0.1,"z":-0.1},{"x":0.1,"y":0.1,"z":0.1},{"x":0.0,"y":0.0,"z":0.0})
+Bfield = SampleFunc({"x":-0.1,"y":-0.1,"z":-0.1},{"x":0.1,"y":0.1,"z":0.1},{"x":0.0,"y":0.05,"z":0.0})
 
-elif kind == "p":
-    q = 1.6/10000000000000000000
-    m = 1.673/1000000000000000000000000000
-
-elif kind == "Al":
-    q = 3*1.6/10000000000000000000
-    m = (26.98/Na)/1000
-
-else:
-    val = int(input("価数を入力してください"))
-    q = val*1.6/10000000000000000000
-    mass = float(input("原子量を入力してください[g/mol]"))
-    m = (mass/Na)/1000
-
-Efield = SampleFunc({"x":-500.0,"y":-500.0,"z":-500.0},{"x":500.0,"y":500.0,"z":500.0},{"x":0.0,"y":0.0,"z":0.1})
-Bfield = SampleFunc({"x":-500.0,"y":-500.0,"z":-500.0},{"x":500.0,"y":500.0,"z":500.0},{"x":0.0,"y":0.0,"z":0.0})
-
-t_max = float(input("計算する時間を入力してください。[s]"))
-dt = 0.00001
+t_max = 0.000005
+dt = 0.0000000005
 t_repetition = t_max/dt
 t_repetition = int(t_repetition)
 
-print("初期位置を入力してください。[m]")
-x0 = list(map(float, input().split()))
+x0 = [0.0, 0.0, -0.2]
 x = np.empty((0,3), float)
 x = np.append(x, np.array([x0]), axis=0)
-print("初速度を入力してください。[m/s]")
-v0 = list(map(float, input().split()))
+v0z = 0.001*299792458
+v0 = [0, 0, v0z]
 v = np.empty((0,3), float)
 v = np.append(v, np.array([v0]), axis=0)
 
@@ -78,11 +61,15 @@ for i in range(t_repetition):
 np.save('runge_posi',x)
 
 #2次元配列からそれぞれの座標を取り出す。
+#グラフプロットをmm単位で。
 X = np.array([x[i][0] for i in range(t_repetition +1)])
+X = 1000*X
 Y = np.array([x[i][1] for i in range(t_repetition +1)])
+Y = 1000*Y
 Z = np.array([x[i][2] for i in range(t_repetition +1)])
+Z = 1000*Z
 
-plams = {"title":"runge","x":X,"y":Y,"z":Z}
+plams = {"title":"Al_0.1%","x":X,"y":Y,"z":Z}
 
 oput = OutPut(plams)
 oput.Show()
