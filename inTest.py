b4343ac7709c6a08ad5305500fa93338a38d4c22
tou-fuu <f.tj.0121@jmail.com>
2020-09-10 20:24:49 +0900

    inp

--- inTest.py
+++ inTest.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import math
 import project as pj
@@ -27,32 +26,3 @@ plams = {"title":"Al_0.01%","x":X,"y":Y,"z":Z}
 
 oput = pj.OutPut.OutPut(plams)
 oput.Show()
-=======
-import numpy as np
-import math
-import project as pj
-
-sub = pj.subtool
-Efield = sub.SampleFunc({"x":-0.1,"y":-0.1,"z":-0.1},{"x":0.1,"y":0.1,"z":0.1},{"x":0.0,"y":0.0,"z":0.0})
-Bfield = sub.SampleFunc({"x":-0.1,"y":-0.1,"z":-0.1},{"x":0.1,"y":0.1,"z":0.1},{"x":0.0,"y":0.005,"z":0.0})
-
-I = pj.InPut
-q, m, t_repetition, x, v = I.inputCSV()
-
-#以下ルンゲクッタ法。
-x, v = sub.runge(Efield, Bfield, q, m, t_repetition, x, v)
-
-#2次元配列からそれぞれの座標を取り出す。
-#グラフプロットをmm単位で。
-X = np.array([x[i][0] for i in range(t_repetition +1)])
-X = 1000*X
-Y = np.array([x[i][1] for i in range(t_repetition +1)])
-Y = 1000*Y
-Z = np.array([x[i][2] for i in range(t_repetition +1)])
-Z = 1000*Z
-
-plams = {"title":"Al_0.01%","x":X,"y":Y,"z":Z}
-
-oput = pj.OutPut.OutPut(plams,Efield,Bfield)
-oput.Show()
->>>>>>> master
