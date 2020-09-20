8dde04ffc65d65ee371a402300bf1245d1124230
Takeuchi-yuya <takei.yuyuyu@gmail.com>
2020-09-20 16:59:52 +0900

    調整

--- functions/makeEFieldMap.py
+++ functions/makeEFieldMap.py
@@ -6,14 +6,16 @@ import scipy.constants as cons
 #3次元プロットするためのモジュール
 from mpl_toolkits.mplot3d import Axes3D
 #行列N×N
-N = 10
+N = 100
 #演算範囲
 lim = 0.5
 #刻み幅
 dr = lim*2/N
 #真空中の誘電率
 e = 8.854e-12
-rho = 1*cons.e
+rho = 10**(-4)*cons.e
+#系内の最大の電位を入れる変数．ある有限の値を入れておく(ゼロ割り防止)
+MaxPhi = 1.0e-10
 def m2Dex(m):
     return int((m + lim)/dr)
 
@@ -25,7 +27,7 @@ phimap = rhomap.copy()
 #flagmap = [[0 for j in range(N)] for i in range(N)]
 rholist = []
 #とりあえず、y=0.1,-0.1,-0.1<x<0.1のとこに電荷置く
-for x in np.arange(-lim,lim,dr):
+for x in np.arange(-0.1,0.1,dr):
     xDex = m2Dex(x)
     yDex = m2Dex(0.1)
     rholist.append([xDex,yDex,rho])
@@ -36,9 +38,8 @@ for x in np.arange(-lim,lim,dr):
     rhomap[yDex][xDex] = -rho
     #flagmap[yDex][xDex] = 2
 #print(rhomap)
-flag = True
 count = 0
-changecount = 0
+'''
 #外周の電位を計算して入れておく
 def calc(pos):
     phi = 0
@@ -54,27 +55,28 @@ for i in range(N):
     phimap[N-1][i] = calc([N-1,i])
     phimap[i][0]   = calc([i,0])
     phimap[i][N-1] = calc([i,N-1])
-
-while flag:
+'''
+MaxErr = 1
+Conv = 10**(-6)
+while MaxErr>Conv:
     #print(phimap)
+    MaxErr = CurErr = 0.0
     count = 1 + count
     flag = False
     for x in range(1,N-1):
         for y in range(1,N-1):
+            Prev_phi = phimap[y][x]
             tmp =1/4 * (rhomap[y][x]*dr*dr/e+phimap[y+1][x]+phimap[y-1][x]+phimap[y][x+1]+phimap[y][x-1])
-            if tmp == float("inf"):
-                print("test")
-                continue
-            if phimap[y][x] != tmp:
-                phimap[y][x] = tmp
-                flag = True
-                changecount = 1 + changecount
-    if count % 100 == 0:
-        print("count",count)
-        print("changecount",changecount)
-        changecount = 0
+            if MaxPhi < abs(tmp):
+                MaxPhi = abs(tmp)
+            phimap[y][x] = tmp
+            CurErr = (abs(phimap[y][x] - Prev_phi))/MaxPhi
+            if MaxErr < CurErr:
+                 MaxErr = CurErr
+
+
+print(count)
         #print(phimap)
-        flag = False
 xlist = np.array([])
 ylist = np.array([])
 philist = np.array([])
@@ -92,6 +94,7 @@ ax = Axes3D(fig)
 #linestyle='None'にしないと初期値では線が引かれるが、3次元の散布図だと大抵ジャマになる
 #markerは無難に丸
 ax.plot(xlist,ylist,philist,marker="o",linestyle='None',markersize=1)
+
 
 #最後に.show()を書いてグラフ表示
 plt.show()
