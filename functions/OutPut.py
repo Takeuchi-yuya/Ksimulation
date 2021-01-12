e047c13842d95f5b81706961dc6842d7663031a2
Takeuchi-yuya <takei.yuyuyu@gmail.com>
2021-01-12 15:23:59 +0900

    修正

--- functions/OutPut.py
+++ functions/OutPut.py
@@ -59,8 +59,10 @@ class OutPut():
 
 
     def twoDPlot(self,ax,losAxis,value = ""):
+        #UnboundLocalErrorがでたので初期化
         horizontal_name = ""
         vertical_name = ""
+        
         for x,y,z,title in zip(self.x,self.y,self.z,self.title):
             horizontal_list = np.array([])
             vertical_list = np.array([])
